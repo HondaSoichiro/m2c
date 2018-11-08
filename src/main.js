acdfb78e4327685a0e8df0e75fb2fe09cee236f9
Sou <suta1218@yahoo.co.jp>
2018-11-08 23:29:48 +0900

    PWA化するためにvue-cli3にアップグレードしpwaプラグイン読み込み

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import App from './App'
 import router from './router'
 import firebase from 'firebase'
 import VueRippler from 'vue-rippler'
+import './registerServiceWorker'
 
 Vue.use(VueRippler)
 
